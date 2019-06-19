a7d3bb8c719ff2717f1614513193fbfddf0d6ce6
Charles Fulnecky <charles.fulnecky@hbo.com>
2019-06-19 11:05:23 -0400

    Remove comments

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -4,14 +4,12 @@ import { ActiveTheme, ThemeList } from '../themes';
 import { LoginForm } from './components/formik';
 import { Logo } from './components/logo';
 
-const black = '#000';
 const App = () => {
   return (
       <ActiveTheme>
           <div className={styles.app}>
               <header className={styles.appHeader}>
                   <Logo className={styles.appLogo} />
-                  {/*<img src="logo.svg" className={styles.appLogo} alt="logo" />*/}
                   <h1 className={styles.appTitle}>CSS Modules with CSS Custom Properties (variables)</h1>
                   <p>select a theme</p>
               </header>
