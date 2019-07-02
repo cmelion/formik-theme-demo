ff7df981a44f9b853a17b17d3ce9642adf732526
Charles Fulnecky <charles.fulnecky@hbo.com>
2019-07-02 17:53:12 -0400

    When plan is approved, go ahead and display all the read-only fields at once, if it gets to be to much use tabs. Stay with wizard for for editing un-approved plans.

--- src/app/components/promo/plan/PromoPlanWizard.jsx
+++ src/app/components/promo/plan/PromoPlanWizard.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import Box from '@material-ui/core/Box';
 import Button from "@material-ui/core/Button";
-import { Formik } from "formik";
+import {Formik} from "formik";
 import Paper from "@material-ui/core/Paper";
-import { default as PromoPlanFields } from "./sections/PromoPlanOverview";
-import { default as PromoCodeDeliveryFields } from "./sections/PromoCodeDeliveryDetails";
-import { validationSchema } from "./schema";
+import {default as PromoPlanFields} from "./sections/PromoPlanOverview";
+import {default as PromoCodeDeliveryFields} from "./sections/PromoCodeDeliveryDetails";
+import {validationSchema} from "./schema";
 import styles from './styles.module.scss';
 
 export const submitValues = ({planName, password}) => {
@@ -24,6 +24,7 @@ const PromoPlanWizard = (props) => {
                 >
                     {props => {
                         const {
+                            values: {approved},
                             errors,
                             handleSubmit,
                             handleChange,
@@ -38,45 +39,53 @@ const PromoPlanWizard = (props) => {
                             setFieldTouched(name, true, false);
                         };
                         const steps = [
-                            <PromoPlanFields         {...props} change={change}/>,
-                            <PromoCodeDeliveryFields {...props} change={change}/>
+                            <PromoPlanFields         key={1} {...props} change={change}/>,
+                            <PromoCodeDeliveryFields key={2} {...props} change={change}/>
                         ];
                         return (
                             <form onSubmit={handleSubmit} className="promo-form">
-                                {steps[step]}
-                                <p/>
-                                <Box display="flex" flexDirection="row">
-                                    {step > 0 ? (
-                                        <Button
-                                            onClick={() => setStep(step - 1)}
-                                            fullWidth
-                                            variant="contained"
-                                            color="primary"
-                                        >
-                                            Back
-                                        </Button>
-                                    ) : null}
-                                    {step === steps.length - 1 ? (
-                                        <Button
-                                            type="submit"
-                                            fullWidth
-                                            variant="contained"
-                                            color="primary"
-                                            disabled={!isValid}
-                                        >
-                                            Submit
-                                        </Button>
-                                    ) : (
-                                        <Button
-                                            onClick={() => setStep(step + 1)}
-                                            fullWidth
-                                            variant="contained"
-                                            color="primary"
-                                        >
-                                            Next
-                                        </Button>
-                                    )}
-                                </Box>
+                                {approved ? (
+                                    <>
+                                        {steps}
+                                    </>) : (
+                                    <>
+                                        {steps[step]}
+                                        <p/>
+                                        <Box display="flex" flexDirection="row">
+                                            {step > 0 ? (
+                                                <Button
+                                                    onClick={() => setStep(step - 1)}
+                                                    fullWidth
+                                                    variant="contained"
+                                                    color="primary"
+                                                >
+                                                    Back
+                                                </Button>
+                                            ) : null}
+                                            {step === steps.length - 1 ? (
+                                                <Button
+                                                    type="submit"
+                                                    fullWidth
+                                                    variant="contained"
+                                                    color="primary"
+                                                    disabled={!isValid}
+                                                >
+                                                    Submit
+                                                </Button>
+                                            ) : (
+                                                <Button
+                                                    onClick={() => setStep(step + 1)}
+                                                    fullWidth
+                                                    variant="contained"
+                                                    color="primary"
+                                                >
+                                                    Next
+                                                </Button>
+                                            )}
+                                        </Box>
+                                    </>
+                                )}
+
                             </form>
                         )
                     }
